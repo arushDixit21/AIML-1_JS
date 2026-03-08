f3ecba7173fcb613a8ed551a9fa3c8138e0fea95
arushDixit21 <arushdixitapsb@gmail.com>
2026-03-08 13:24:23 +0530

    javascript

---
+++ script.js
@@ -0,0 +1,129 @@
+// console.log("Hello World!");
+//document.write("Hello World!");
+// var num=10;
+// console.log(num);
+// console.log(type(num));
+// num=false;
+// console.lof(num);
+// console.log(typed(num));
+// num="Sanjeev soni";
+// console.log(num);
+// console.log(typed(num));
+// num=null;
+// console.log(num);
+// console.log(typed(num));
+// num=undefined;
+
+// var a=10;
+// var b=20;
+// var linebreak="</br>";
+
+// document.write("(a==b)=>");
+// result=(a==b);
+// document.write(result);
+// document.write(linebreak);
+// document.write("(a>b)=>");
+// result=(a>b);
+// document.write(result);
+// document.write(linebreak);
+
+/*to find max of any of the three numbers given -Assignment 1*/
+
+// var count;
+// document.write("Starting Loop"+"<br/>");
+// for(count=0; count<0; count++){
+//     document.write("Current count:" + count);
+//     document.write("<br/>");
+// }
+// document.write("loop stopped");
+
+// for(key in object){
+//     //for block to be executed
+// }
+// const person={
+//     fname:"Ajay",
+//     Iname:"Singh",
+//     age:25,
+// };
+// for(let x in person){
+//     console.log("person details:" + x + ";" + person[x])
+// }
+
+// var i;
+// for(i=0; i<3; i=i+1)
+// {
+//     console.log(i);
+// }
+// for(x in object){
+// //code block to be executed
+// }
+// const person= {
+// fname:"Ajay" ,
+//  lname:"Singh",
+// age:25,
+// };
+// for(let x in person){
+//     console.log("person details:" +x+";"+person.x)
+
+// }
+
+// var count =0;
+// document.write("Starting Loop");
+
+// while(count<10){
+//     document.write("Current Count"+ count+ "<br/>");
+//     count ++;
+// }
+
+// document.write("loop stopped");
+
+// let age=20;
+
+// if(age>=18){
+//     console.log("You are an adult:");
+// }
+// else{
+//     console.log("You are a minor");
+// }
+
+// var grade='A';
+// document.write("Enter switch block </br>");
+// switch(grade){
+//     case 'A': document.write("Good job</br>");
+//     break;
+
+//    case 'B' : document.write("Pretty good</br>");
+//    break;
+    
+//    case 'C' : document.write("Passed</br>");
+//    break;
+
+//    case 'D' : document.write("Not tp good</br>");
+//    break;
+
+//    default: document.write("Unknown grade</br>")
+// }
+
+// document.write("Existing switch block");
+
+
+function myFunction()
+{
+    alert("Hello World;")
+}
+<button onClick="myFunction()">Try it</button>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
